65cecf4503163961c77025e30877f06d47586e03
Etjen Buljubašić <buljubasicetjen4@gmail.com>
2024-01-22 22:28:19 +0100

    compilation test, added postion and token structs

--- compiler.h
+++ compiler.h
@@ -2,6 +2,50 @@
 #define EBCCOMPILER_H
 
 #include <stdio.h>
+#include <stdbool.h>
+
+struct position
+{
+    int line;
+    int col;
+    const char* filename;
+};
+
+enum{
+    TOKEN_TYPE_IDENTIFIER,
+    TOKEN_TYPE_KEYWORD,
+    TOKEN_TYPE_OPERATOR,
+    TOKEN_TYPE_SYMBOL,
+    TOKEN_TYPE_NUMBER,
+    TOKEN_TYPE_STRING,
+    TOKEN_TYPE_COMMENT,
+    TOKEN_TYPE_NEWLINE
+};
+
+struct token
+{
+    int type;
+    int flags;
+
+    union
+    {
+        char cval;
+        const char* sval;
+        unsigned int inum;
+        unsigned long lnum;
+        unsigned long long llnum;
+        void* any;
+    };
+    
+    //true if there is whitespace between the token and the next token
+    //i.e. * a for operator token * would mean whitespace would be set for token a
+    bool whitespace;
+
+    //this would point to a list of charaters(string)
+    //for example, if we have (66+77+1000) the value of between_brackets would be a string "66+77+1000"
+    const char* between_brackets;
+};
+
 
 enum{
     COMPILER_FILE_COMPILED_OK,
--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include "compiler.h"
 
 int main(){
-    printf("Hello world!\n");
+    int res = compile_file("./test.c", "./test", 0);
+    if (res == COMPILER_FILE_COMPILED_OK)
+    {
+        printf("Compilation succesful.\n");
+    }
+    else
+    {
+        printf("Compilation failed.\n");
+    }
+    
     return 0;
 }
